1c6b2f2ffe37a176ece15c7a5178205edef87f39
realakshay <gaikwadakshay663@gmail.com>
2020-04-25 09:00:14 +0530

    feedback created

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api
 from flask_jwt import JWT
 from security import authenticate, identity
@@ -16,10 +16,23 @@ jwt=JWT(app, authenticate, identity)
 '''@app.before_first_request
 def create_tables():
     db.create_all()'''
+feedbacks=[]
 
 @app.route('/')
 def index():
     return "<h3>Application started..</h3>"
+
+@app.route('/feedback', methods=['POST'])
+def post_feedback():
+    data=request.get_json()
+    feedbacks.append(data['feedback'])
+    return {'feedback':data['feedback']}
+
+@app.route('/feedback', methods=['GET'])
+def get_feedback():
+    return {'feedbacks':feedbacks[-3:]}
+    
+
 
 api.add_resource(Employee, '/employee/<string:empid>')
 api.add_resource(Role, '/role/<string:name>')
